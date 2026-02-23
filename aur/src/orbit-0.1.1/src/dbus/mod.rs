fbbf6811a38e325b6eb46bd59a372fc536e1df2f
LifeOfATitan <abdoulisme@gmail.com>
2026-02-23 00:57:43 -0500

    Update AUR package to v0.1.2

--- aur/src/orbit-0.1.1/src/dbus/mod.rs
+++
@@ -1,5 +0,0 @@
-pub mod network_manager;
-pub mod bluez;
-
-pub use network_manager::{NetworkManager, AccessPoint, SecurityType, SavedNetwork, NetworkDetails};
-pub use bluez::{BluetoothManager, BluetoothDevice, DeviceType};
